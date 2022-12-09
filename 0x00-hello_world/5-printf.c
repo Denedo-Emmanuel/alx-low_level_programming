9fa45cbd46291510e9b196c1100aca43f815eaf5
Denedo-Emmanuel <denedoemmanuel6@gmail.com>
2022-12-09 10:32:37 -0800

    ajsgag

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
 	 * Description : use printf
 	 * Return: 0
 	 */
+
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
