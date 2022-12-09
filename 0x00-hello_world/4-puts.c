b0c72e5ac23125944ea27847b6cb2a8c063fa080
Denedo-Emmanuel <denedoemmanuel6@gmail.com>
2022-12-09 09:14:30 -0800

    hdjdos

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ *Main - Entry point
  *Description: prints exactly "Programming is like building a multilingual puzzle"
- *return: o
+ *Return: o
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
