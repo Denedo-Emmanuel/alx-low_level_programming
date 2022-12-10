7fbf7627a30097a128569b3aba7342c4f8eb21b0
Denedo-Emmanuel <denedoemmanuel6@gmail.com>
2022-12-10 04:41:59 -0800

    gkohx

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - print sizes of various types of computers
+ * Description : print the exact sizes of various types of computers.
+ * Return : o
+ */
+int main(void)
+{
+	printf("size of a char: %zu byte(s)\n", size of(char));
+	printf("size of an int: %zu byte(s)\n", size of(int));
+	printf("size of a long int: %zu byte(s)\n", size of(long int));
+	printf("size of a long long int: %zu byte(s)\n", size of(long long int));
+	printf("size of a float: %zu byte(s)\n", size of(float));
+	return (0);
+}
+
