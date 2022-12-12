5d9521be221c77dd5e277ed1c49008e7fa27140a
Denedo-Emmanuel <denedoemmanuel6@gmail.com>
2022-12-11 22:47:13 -0800

    alpha

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Descroption: program prints all single digitsfrom 0 in base 10\n
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
