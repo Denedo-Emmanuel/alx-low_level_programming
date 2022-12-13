70c9b17053f97b46e062101ed415dac3ceeb6437
Denedo-Emmanuel <denedoemmanuel6@gmail.com>
2022-12-13 13:29:48 -0800

    my mum

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: program prints "_putchar" followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This directory 0x02-functions_nested_loops contains files for learning functions, nesting and loops
+
