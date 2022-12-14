4dd4d33df680ecb4818c5af93cac45ab298a8052
Denedo-Emmanuel <denedoemmanuel6@gmail.com>
2022-12-14 05:40:15 -0800

    able

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: print all alphabet in lowercase \n
+ * Return: void
+ */
+void print_alphabet(void);
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,4 @@
+#
 #define _main_h_
 int _putchar(char);
+void print_alphabet(void);
